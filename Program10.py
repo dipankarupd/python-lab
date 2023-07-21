4dfcd3b2ee38a8b0fc3703c7219d4f8b3ad98fa2
Dipankar Raj Upadhyaya <drupd17@gmail.com>
2023-07-21 06:59:28 +0530

    Update code

--- Program10.py
+++ Program10.py
@@ -2,8 +2,9 @@ fo = open("file1.txt", "r")
 newFile = open("result.txt", "w")
 
 lines = fo.readlines()
-print(len(lines))
+
 for i in range(len(lines)):
+    
     if (i+1) % 2 != 0:
         newFile.write(lines[i])
 
--- try.py
+++
@@ -1,3 +0,0 @@
-arr = [i for i in range (97, 97+26+1)]
-print(arr)
-print(chr(arr[0]))
